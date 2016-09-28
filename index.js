2c13a939da501d33d1faf4472a8b15459b233255
Denis Karabaza <denis.karabaza@gmail.com>
2016-09-28 22:48:18 +0300

    Warn when incompatible version of Vue is used

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import Vue from 'vue';
+
+export var version = '2.0.0-rc1';
+
+var compatible = (/^2\./).test(Vue.version);
+if (!compatible) {
+  Vue.util.warn('VueFocus ' + version + ' only supports Vue 2.x, and does not support Vue ' + Vue.version);
+}
+
 export var focus = {
   inserted: function(el, binding) {
     if (binding.value) el.focus();
