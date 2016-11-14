976413d1a07e6eaaa42863754d5b8198ef394662
Denis Karabaza <denis.karabaza@gmail.com>
2016-11-15 00:53:17 +0300

    Add a new `.lazy` modifier to mimic behavior of v1

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ export var focus = {
   },
 
   componentUpdated: function(el, binding) {
+    if (binding.modifiers.lazy) {
+      if (Boolean(binding.value) === Boolean(binding.oldValue)) {
+        return;
+      }
+    }
+
     if (binding.value) el.focus();
     else el.blur();
   },
